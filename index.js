3ffdd8c0161f8517597e656462c500e0cb0829a9
chaim <chaimyisroel@gmail.com>
2021-08-11 16:56:46 -0400

    commit tree b6b2f0abd9d4bec56144f9b4f9a571eceb0539c2

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(record) {
+    const found = record.find( item => item.result === 'W')
 
+    if (found !== undefined) {
+    return found.year
+    } else {
+        return undefined
+    }
+}
